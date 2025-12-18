e250b044ceb6b999462ce897784b5450c55fe0f1
Frodo <marav.devpro@gmail.com>
2025-12-18 11:41:15 -0500

    dev04
    prototipo 5
    se agrega routing y se muestra app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: '',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-
+  title = signal('manjaro.fe00');
 }
